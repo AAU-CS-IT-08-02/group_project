b5a078f6936045a4a9760e028878370b258633ea
Akritibali2025 <baliakriti6@gmail.com>
2026-05-25 17:31:12 +0200

    Updated pursuer and evader agents

--- evader.py
+++ evader.py
@@ -2,133 +2,44 @@ import numpy as np
 import random
 from collections import defaultdict
 
-from env import (
-    NUM_ACTIONS
-)
+from env import NUM_ACTIONS
+from base_agent import BaseAgent
 
 
-class Evader:
+class AGENT_CLASS(BaseAgent):
 
-    def __init__(
+    def __init__(self,
+                 alpha=0.1,
+                 gamma=0.95,
+                 epsilon=1.0,
+                 epsilon_decay=0.995,
+                 epsilon_min=0.01):
 
-        self,
-
-        alpha=0.1,
-
-        gamma=0.95,
-
-        epsilon=1.0,
-
-        epsilon_decay=0.995,
-
-        epsilon_min=0.01
-
-    ):
-
-        self.Q = defaultdict(
-            lambda:
-            np.zeros(
-                NUM_ACTIONS
-            )
-        )
+        self.Q = defaultdict(lambda: np.zeros(NUM_ACTIONS))
 
         self.alpha = alpha
-
         self.gamma = gamma
 
         self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
 
-        self.epsilon_decay = (
-            epsilon_decay
-        )
+    def choose_action(self, state):
 
-        self.epsilon_min = (
-            epsilon_min
-        )
+        if random.random() < self.epsilon:
+            return random.randint(0, NUM_ACTIONS - 1)
 
-    def choose_action(
-        self,
-        state
-    ):
+        return np.argmax(self.Q[state])
 
-        if (
-            random.random()
-            <
-            self.epsilon
-        ):
+    def update(self, state, action, reward, next_state, next_action):
 
-            return random.randint(
-                0,
-                NUM_ACTIONS - 1
-            )
+        current = self.Q[state][action]
 
-        return np.argmax(
-            self.Q[state]
-        )
+        target = reward + self.gamma * self.Q[next_state][next_action]
 
-    def update(
-
-        self,
-
-        state,
-
-        action,
-
-        reward,
-
-        next_state,
-
-        next_action
-
-    ):
-
-        current = (
-            self.Q[state][action]
-        )
-
-        target = (
-
-            reward
-
-            +
-
-            self.gamma
-
-            *
-
-            self.Q[
-                next_state
-            ][
-                next_action
-            ]
-        )
-
-        self.Q[state][action] += (
-
-            self.alpha
-
-            *
-
-            (
-
-                target
-
-                -
-
-                current
-
-            )
-        )
+        self.Q[state][action] += self.alpha * (target - current)
 
     def decay(self):
 
-        self.epsilon = max(
-
-            self.epsilon_min,
-
-            self.epsilon
-
-            *
-
-            self.epsilon_decay
-        )
+        self.epsilon = max(self.epsilon_min,
+                           self.epsilon * self.epsilon_decay)
